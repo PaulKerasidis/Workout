44f8992acac413bdab0da6fd0086e2369443b2db
Pkerasidis <P.kerasidis@icloud.com>
2024-09-15 16:23:08 +0300

    expo navigation installed

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -7,7 +7,7 @@ export default function ExerciseListItem({item}){
           {item.name}
         </Text>
         <Text style= {styles.exerciseSubtitle}>
-          {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+          {item.muscle} | {item.equipment}
         </Text>
       </View>
     )
@@ -20,12 +20,23 @@ export default function ExerciseListItem({item}){
       padding: 10,
       borderRadius: 10,
       gap: 5,
+      marginHorizontal: 2,
+      // Shadow 
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+        },
+      shadowOpacity: 0.20,
+      shadowRadius: 1.41,
+      elevation: 2,
       },
     exerciseName:{
       fontSize: 20,
       fontWeight: '500',
     },
     exerciseSubtitle:{
-      color: 'dimgray'
+      color: 'dimgray',
+      textTransform: 'capitalize'
     }
   });
